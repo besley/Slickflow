d9ce5ecfcc24d56eb327b8ffe97ac173edb2cd1a
besley <william.ligong@yahoo.com>
2019-01-10 21:16:18 +0800

    updated SfOL slcikflow open license comments.

--- NET/Slickflow/Source/Slickflow.Designer/ViewJS/kloader.js
+++ NET/Slickflow/Source/Slickflow.Designer/ViewJS/kloader.js
@@ -1,23 +1,20 @@
 ﻿/*
-* Slickflow 开源项目遵循LGPL协议，也可联系作者获取企业版商业授权和技术支持；
+* Slickflow 软件遵循自有项目开源协议，也可联系作者获取企业版商业授权和技术支持；
 * 除此之外的使用则视为不正当使用，请您务必避免由此带来的一切商业版权纠纷和损失。
-
-The Slickflow Designer project.
+* 
+The Slickflow Open License (SfPL 1.0)
 Copyright (C) 2014  .NET Workflow Engine Library
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, you can access the official
-web page about lgpl: https://www.gnu.org/licenses/lgpl.html
+1. Slickflow software must be legally used, and should not be used in violation of law, 
+   morality and other acts that endanger social interests;
+2. Non-transferable, non-transferable and indivisible authorization of this software;
+3. The source code can be modified to apply Slickflow components in their own projects 
+   or products, but Slickflow source code can not be separately encapsulated for sale or 
+   distributed to third-party users;
+4. The intellectual property rights of Slickflow software shall be protected by law, and
+   no documents such as technical data shall be made public or sold.
+5. The enterprise, ultimate and universe version can be provided with commercial license, 
+   technical support and upgrade service.
 */
 
 var kloader = (function () {
