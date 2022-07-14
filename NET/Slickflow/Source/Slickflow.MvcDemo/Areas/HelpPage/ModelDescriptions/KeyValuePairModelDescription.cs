71a36577d18e97764ba4bb2c4a51cce3dc0769d7
Besley <william.ligong@hotmail.com>
2022-07-15 07:25:02 +0800

    slickflow product has been upgraded to BPMN2

--- NET/Slickflow/Source/Slickflow.MvcDemo/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace Slickflow.MvcDemo.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
--- NET/Slickflow/Source/Slickflow.MvcDemo/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Slickflow.MvcDemo.MvcApplication" Language="C#" %>
